6077bf7b99accfe258a1ec0dcf71be7ec95cc00a
godchisa254 <Matias.254@outlook.com>
2025-11-01 21:22:30 -0300

    build(program); adds main services and environment initialziation to main program.cs script

--- Program.cs
+++ Program.cs
@@ -1,11 +1,22 @@
+using censudex_auth_service.src.Extensions;
+using censudex_auth_service.src.Repositories;
+using censudex_auth_service.src.Services;
+using DotNetEnv;
 
+Env.Load();
 var builder = WebApplication.CreateBuilder(args);
-  
- 
+
+var jwtSecret = Environment.GetEnvironmentVariable("JWT_SECRET") ?? throw new InvalidOperationException("JWT_SECRET environment variable is not set");
+var clientsUrl = Environment.GetEnvironmentVariable("CLIENTS_SERVICE_URL");
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-  
+
+builder.Services.AddHttpClient<IAuthService, AuthService>();
+builder.Services.AddSingleton<ITokenBlockListRepository, InMemoryTokenBlockListRepository>();
+builder.Services.ConfigureJwtAuthentication(jwtSecret);
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
